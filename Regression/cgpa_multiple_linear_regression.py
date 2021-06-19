ec951ebdf38f36f3f2b9509598574b25b1084f32
Utkarsh Verma <68218947+vermautkarsh44@users.noreply.github.com>
2021-06-19 17:40:29 +0530

    Update cgpa_multiple_linear_regression.py

--- Regression/cgpa_multiple_linear_regression.py
+++ Regression/cgpa_multiple_linear_regression.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #importing dataset
-dataset=pd.read_csv("Student's Information_cse - Copy.csv")
+dataset=pd.read_csv("Student's Information_cse.csv")
 X= dataset.iloc[:,:12].values
 Y=dataset.iloc[:,12].values
 
